2c74bb137f9eba3189f6a107ca6d2a75cca09450
Marty <coolkidmarty@gmail.com>
2022-11-02 21:11:32 +0000

    menu

--- src/Menu.java
+++ src/Menu.java
@@ -7,7 +7,7 @@ public class Menu {
         // possible solution to food item?
         FoodItem pizza = new FoodItem('M', "Pizzas", "pizza, dish of Italian origin consisting of a flattened disk of bread dough topped with some combination", 20.0);
         FoodItem burger = new FoodItem('M', "MickDouble", "6oz meat patty between two brioche buns with cheese, lettuce and onions ", 13.0);
-        FoodItem burger = new FoodItem('D', "MickFlurry", "soft serve Mick's vanilla-flavored ice cream in a cup.", 3.0);
+        FoodItem MickFlurry = new FoodItem('D', "MickFlurry", "soft serve Mick's vanilla-flavored ice cream in a cup.", 3.0);
 
 
         System.out.println("Cost of pizza = " + pizza.getPrice());
