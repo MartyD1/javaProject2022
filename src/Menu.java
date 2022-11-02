a745da2ac32c21faa94a04669f6d81c8c6e14203
Noel O'Gorman <noelogorman2812@gmail.com>
2022-11-02 17:14:24 +0000

    test test

--- src/Menu.java
+++ src/Menu.java
@@ -5,7 +5,7 @@ public class Menu {
     public static void main(String[] args) {
 
         // possible solution to food item?
-        FoodItem pizza = new FoodItem('M', "Pizza", "pizza, dish of Italian origin consisting of a flattened disk of bread dough topped with some combination", 15.0);
+        FoodItem pizza = new FoodItem('M', "Pizza", "pizza, dish of Italian origin consisting of a flattened disk of bread dough topped with some combination", 20.0);
 
 
         System.out.println("Cost of pizza = " + pizza.getPrice());
