ca075b685a8ca49e2b92c07c920dda4b8642d8ab
valcluney <117280636+valcluney@users.noreply.github.com>
2022-11-03 19:46:36 +0000

    added tables which can be repositioned uniformly

--- src/RestaurantGUI.java
+++ src/RestaurantGUI.java
@@ -10,7 +10,7 @@ public class RestaurantGUI extends JPanel {
     //Declaring instance variables for restaurant tables
     private  int tablewidth = 150;
     private  int tablelength = 100;
-    //Decalring instance variables for x and y pos
+    //Declaring instance variables for x and y pos
     private int xPos=100;
     private int yPos=100;
     @Override
@@ -24,15 +24,28 @@ public class RestaurantGUI extends JPanel {
         g.fillRect(0,0,screenWidth,screenLength);
 
         //tables
-        //for(int i=1; i<=5;i++){
-       //     g.setColor(Color.GREEN);
-       //     g.fillRect(xPos * 3*i, yPos, tablewidth, tablelength);
-       // }
-        for(int i=1; i<=5;i++) {
-            JButton table1 = new JButton("Table"+i);
-            table1.setBounds(xPos * 3*i , yPos, tablewidth, tablelength);
-            table1.setBackground(Color.GREEN);
-            add(table1);
+        for(int i=1; i<=12;i++) {
+            JButton table = new JButton("Table" + i);
+            table = new JButton("Table" + i);
+            if(i<5) {
+               table.setBounds(xPos * i *3, yPos, tablewidth, tablelength);
+            }
+            table.setBackground(Color.GREEN);
+            add(table);
+            if((i>=5)&&(i<9)){
+               table = new JButton("Table" + (i));
+               table.setBounds(xPos*(i-4) *3 , yPos*3, tablewidth, tablelength);
+            }
+            table.setBackground(Color.GREEN);
+            add(table);
+            if(i>=9){
+
+                table = new JButton("Table" + ( i));
+                table.setBounds(xPos*(i-8)*3 , yPos * 5, tablewidth, tablelength * 2);
+
+            }
+            table.setBackground(Color.GREEN);
+            add(table);
         }
 
     }
@@ -46,4 +59,4 @@ public class RestaurantGUI extends JPanel {
         frame.setExtendedState(JFrame.MAXIMIZED_BOTH); //maximise window
         frame.setVisible(true);//display frame
     }
-}
+        }
