d78fa227ae55cd5aecc35020f0f19516d01903e7
valcluney <117280636+valcluney@users.noreply.github.com>
2022-11-13 20:26:35 +0000

    fixed

--- src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module com.example.restaurantapplication {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-    requires org.controlsfx.controls;
-
-    opens com.example.restaurantapplication to javafx.fxml;
-    exports com.example.restaurantapplication;
-}
