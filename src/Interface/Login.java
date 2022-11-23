8097f8563bd6f3cf77c00197514bff9d577bec08
Marty <coolkidmarty@gmail.com>
2022-11-23 21:55:30 +0000

    Changes by noelogorman

--- src/Interface/Login.java
+++ src/Interface/Login.java
@@ -3,12 +3,14 @@ package Interface;
 import Person.Customer;
 import Person.Owner;
 import ReservationSystem.Booking;
+import ReservationSystem.BookingCalendar;
 import ReservationSystem.BookingDate;
 import ReservationSystem.BookingTime;
 import RestaurantSystem.Menu;
 import RestaurantSystem.Restaurant;
 import RestaurantSystem.Table;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Scanner;
 
@@ -28,20 +30,20 @@ public class Login {
     Restaurant restaurant = new Restaurant("Yum Restaurant", owner, menu);
     Scanner scan = new Scanner(System.in);
 
-    Login(){
+    Login() throws IOException {
         System.out.println("Enter Username: ");
         userField = scan.nextLine();
         System.out.println("Enter Password: ");
         passField = scan.nextLine();
 
-        if(userField.equals("Guest") && passField.equals("Password")){
+        if (userField.equals("Guest") && passField.equals("Password")) {
             GuestLogin();
         } else if (userField.equals("Staff") && (passField.equals("Password"))) {
             staffMenu();
         } else if (userField.equals("Management") && (passField.equals("Password"))) {
             managementOptions();
 
-        }else {
+        } else {
             System.out.println("Incorrect Login");
             new Login();
 
@@ -53,21 +55,29 @@ public class Login {
     private void managementOptions() {
         System.out.println("(M)anage employees, (V)iew records, (T)ables ");
         String input = scan.nextLine().toUpperCase();
-        switch(input){
-            case("M"):
+        switch (input) {
+            case ("M"):
+                System.out.println("Placeholer, Manage employees");
+                break;
+            case("V"):
+                System.out.println("Placeholder, View records");
+                break;
+            case("T"):
+                TableMenu();
+                break;
 
         }
     }
 
 
-    void restaurantMenu(){
+    void restaurantMenu() {
         System.out.println("Would you like to (C)reate restaurants, (V)iew restaurants?");
         String input = scan.nextLine().toUpperCase();
-        switch(input){
-            case("C"):
+        switch (input) {
+            case ("C"):
                 createRestaurant();
                 break;
-            case("V"):
+            case ("V"):
                 viewRestaurants();
                 break;
         }
@@ -121,8 +131,8 @@ public class Login {
 
     }
 
-    private Object getChoice(Object[] choices) {
-        if (choices.length == 0) return null;
+    private String getChoice(ArrayList<String> choices) {
+        if (choices.size() == 0) return null;
         while (true) {
             char c = 'A';
             for (Object choice : choices) {
@@ -131,11 +141,10 @@ public class Login {
             }
             String input = scan.nextLine();
             int n = input.toUpperCase().charAt(0) - 'A';
-            if (0 <= n && n < choices.length)
-                return choices[n];
+            if (0 <= n && n < choices.size())
+                return choices.get(n);
         }
     }
-
 
 
     private void chefOptions() {
@@ -143,26 +152,31 @@ public class Login {
     }
 
 
-    private void staffMenu() {
-        System.out.println("(M)anagement, (R)eservations, (F)ood");
-        String staffMenu = scan.nextLine().toUpperCase();
-        switch(staffMenu){
-            case("M"):
-                System.out.println("Work in progress");
-                break;
-            case("R"):
-                System.out.println("Work in progress");
-                break;
-            case("F"):
-                System.out.println("Work in progress");
-                break;
+    private void staffMenu() throws IOException {
+        boolean cont = true;
+        System.out.println("Welcome to the Staff Menu!\n" +
+                "Please enter a command to continue.");
+
+        while (cont) {
+            System.out.println("(R)eservations, (F)ood (Q)uit system");
+            String command = scan.nextLine().toUpperCase();
+
+            if (command.equals("R")) {
+                completeBookingMenu();
+
+            } else if (command.equals("F")) {
+
+            } else if (command.equals("Q")) {
+                cont = false;
+            }
+
         }
     }
 
-    void GuestLogin(){
+    void GuestLogin() {
         boolean cont = true;
         System.out.println("Welcome to the Yum Restaurant Chain!\n" +
-                           "Please enter a command to continue.");
+                "Please enter a command to continue.");
 
         while (cont) {
             System.out.println("(B)ook a reservation, (V)iew menu, (P)ay a bill, (Q)uit system");
@@ -171,16 +185,35 @@ public class Login {
             if (command.equals("B")) {
                 addBooking();
                 System.out.println();
-            }
-            else if (command.equals("V")) {
+            } else if (command.equals("V")) {
                 showMenu();
                 System.out.println();
-            }
-            else if (command.equals("P")) {
+            } else if (command.equals("P")) {
                 System.out.println("WIP");
                 System.out.println();
+            } else if (command.equals("Q")) {
+                cont = false;
             }
-            else if (command.equals("Q")) {
+        }
+    }
+
+    public void completeBookingMenu() throws IOException {
+        boolean cont = true;
+        BookingCalendar calendar = new BookingCalendar();
+        while (cont) {
+            System.out.println("A)dd booking\nC)ancel booking\nS)how booking\nQ)uit system");
+            String command = scan.nextLine().toUpperCase(); // if user inputs lowercase command, it will still be read
+
+            if (command.equals("A")) { // add booking function follows
+                addBooking();
+
+            } else if (command.equals("C")) {
+                cancelBooking();
+
+            } else if (command.equals("S")) {
+                showBooking();
+
+            } else if (command.equals("Q")) {
                 cont = false;
             }
         }
@@ -188,19 +221,19 @@ public class Login {
 
     private void addBooking() {
         System.out.println("""
-                           Date of booking
-                           (yyyy-mm-dd)""");
+                Date of booking
+                (yyyy-mm-dd)""");
         String dateString = scan.nextLine();
         BookingDate d = new BookingDate(dateString);
 
         System.out.println("""
-                           Time of booking
-                           (hh:mm)""");
+                Time of booking
+                (hh:mm)""");
         String timeString = scan.nextLine();
         BookingTime t = new BookingTime(timeString);
 
         System.out.println("Customer: ");
-        Customer customer=new Customer(scan.nextLine(),scan.nextLine());
+        Customer customer = new Customer(scan.nextLine(), scan.nextLine());
 
         System.out.println("Number of guests: ");
         String numberOfGuests = scan.nextLine();
@@ -217,11 +250,46 @@ public class Login {
         createBookingsCSV(booking); // added booking to CSV file
     }
 
-    private void showMenu() {
+    private void cancelBooking() throws IOException {
+        System.out.println("\nEnter booking details to cancel: ");
+        System.out.println("""
+                Enter date:
+                (yyyy-mm-dd)""");
+        String dateLine = scan.nextLine();
+
+        ArrayList<String> allBookings = readCSV("bookingsRecord.csv");
+        ArrayList<String> choices = getBookingsForDay(dateLine);
+
+        String cancelBooking = getChoice(choices);
+        if (cancelBooking != null) {
+            allBookings.remove(cancelBooking);
+        }
+
+        clearCSV("bookingsRecord.csv");
+
+        for (int i = 0; i < allBookings.size(); i++) {
+            Booking tempBooking = new Booking(allBookings.get(i));
+            createBookingsCSV(tempBooking);
+        }
+    }
+
+    private void showBooking() {
+        System.out.println("""
+                        Display what date:
+                        (yyyy-mm-dd)""");
+        String showDate = scan.nextLine();
+        ArrayList<String> bookingsForDay = getBookingsForDay(showDate);
+
+        for (int i = 0; i < bookingsForDay.size(); i++) {
+            System.out.println(bookingsForDay.get(i));
+        }
+    }
+
+
+    public void showMenu() {
         ArrayList<String> menuItems = readCSV("menuRecord.csv");
 
         for (int i = 0; i < menuItems.size(); i++) {
-            System.out.println(menuItems.get(i));
-        }
+            System.out.println(menuItems.get(i));}
     }
 }
