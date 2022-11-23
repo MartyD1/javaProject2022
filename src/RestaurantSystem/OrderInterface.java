fa209dbfb3d75a92e32390586ff50eb437e646fb
Marty <coolkidmarty@gmail.com>
2022-11-23 21:57:54 +0000

    groupwork test changes

---
+++ src/RestaurantSystem/OrderInterface.java
@@ -0,0 +1,21 @@
+package RestaurantSystem;
+
+import Person.Staff;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+import static CSV.GeneralCSV.clearCSV;
+import static CSV.GeneralCSV.readCSV;
+import static Person.PersonCSV.createPersonCSV;
+import static Person.PersonCSV.getChosenStaff;
+import static RestaurantSystem.MenuCSV.createMenuCSV;
+
+public class OrderInterface {
+    private Scanner in;
+
+
+
+
+}
